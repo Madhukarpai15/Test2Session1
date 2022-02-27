b9586a769e4ff6e02108ec0b100fdb1a74735b5c
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-02-27 14:20:08 +0000

    p5

--- p5original.c
+++ p5original.c
@@ -1,52 +1,57 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <math.h>
-
 int input_array_size()
 {
-  int a;
-  printf("Enter the number of terms: ");
-  scanf("%d",&a);
-  return a;
+  int n;
+  printf("enter the size of the array\n");
+  scanf("%d",&n);
+  return n;
 }
-
 void input_array(int n,int a[n])
 {
-  int i=0;
-  for(i=0;i<n;i++)
+  for(int i=0;i<n;i++)
     {
       a[i]=i;
     }
+  a[1]=0;
 }
-
 void erotosthenes_sieve(int n,int a[n])
 {
-  a[1]=0;
   int i=0;
-  while(i< sqrt(n))
-    {
-      while(a[i]==0)i++;
-      for(int k=i+i;i<n;k+=i)
-        a[k]=0;
-      i++;
+  while (i<sqrt(n))
+  {
+      while(a[i]!=0)
+      {
+        int f;
+        for(f=i+i;f<n;f+=i)
+        {
+         a[f]=0;
+        }
+        break;
+      }
+    i++;  
+  }
+  
 }
-
 void out_put(int n,int a[n])
 {
-  int i=2; 
-  while(a[i]!=0)
+  int i;
+  for(i=2;i<n;i++)
     {
-      printf("%d,",a[i]);
-      i++;
+      while(a[i]!=0)
+        {
+          printf("%d,",a[i]);
+          break;
+        }
     }
 }
-
 int main()
 {
-  int n;
-  n=input_array_size();
-  int a[n],b[n];
-  input_array(n,a);
-  erotosthenes_sieve(n,a);
-  out_put(n,a);
-  
+  int x;
+  x=input_array_size();
+  int y[x];
+  input_array(x,y);
+  erotosthenes_sieve(x,y);
+  out_put(x,y);
+  return 0;
 }
